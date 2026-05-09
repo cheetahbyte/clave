a41a706ce3e5c6d2b987cc67d9beb7c5ce41688f
Leonhard Breuer <git@leonhardbreuer.de>
2026-05-09 14:44:40 +0200

    feat: add ratelimit

--- backend/internal/api/router.go
+++ backend/internal/api/router.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cheetahbyte/clave/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httprate"
 )
 
 func Register(r *chi.Mux, h *handlers.Handlers) {
@@ -13,20 +14,13 @@ func Register(r *chi.Mux, h *handlers.Handlers) {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(3 * time.Second))
 	r.Use(middleware.Logger)
+	r.Use(httprate.LimitByIP(10, 1*time.Minute))
 
 	r.Route("/api", func(apiRouter chi.Router) {
 		apiRouter.Route("/v1", func(v1Router chi.Router) {
 			v1Router.Post("/activate", h.ActivateLicense)
-			v1Router.Post("/", h.CreateLicense)
+			v1Router.With(h.RequireAdminBearerToken).Post("/", h.CreateLicense)
 			v1Router.Post("/validate", h.ValidateLicense)
-			// self service
-			v1Router.Post("/selfservice/auth/request-token", h.RequestSelfServiceLink)
-			v1Router.Post("/selfservice/auth/validate", h.ValidateSelfServiceToken)
-			v1Router.Route("/selfservice", func(ss chi.Router) {
-				ss.Use(h.RequireSelfServiceAuth)
-				ss.Get("/", h.ListSelfServiceLicenses)
-				ss.Get("/check", h.CheckSelfServiceToken)
-			})
 		})
 	})
 }
