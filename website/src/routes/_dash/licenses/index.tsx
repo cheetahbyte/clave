f511ac9e384104381865728770bf9dff5a8f99ba
Leonhard Breuer <git@leonhardbreuer.de>
2026-06-13 13:52:44 +0200

    build: add dockerfile

--- website/src/routes/_dash/licenses/index.tsx
+++ website/src/routes/_dash/licenses/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { useCurrentProduct } from "@/features/admin/product-context";
 import {
